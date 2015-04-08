45c32125915e04ce6effbdd4a7786abf21af48ad
lucasaxm <lucas_axm@hotmail.com>
2015-04-08 22:17:57 +0000

    matriz chave implementada
    Signed-off-by: lucasaxm <lucas_axm@hotmail.com>

--- crypt.rb
+++ crypt.rb
@@ -1,18 +1,68 @@
-n=ARGV[0].to_i
-matriz= Array.new
-i=0
-j=0
-input = File.open("./input", "r") do |file|
-    file.each_char do |c|
-        if i<n
-            matriz[n*j+(i+=1)]=c
-        else
-            i=0
-            j+=1
-            # matriz[n*j+i]=c
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+# methods{
+    def criaArrayChave(keyword)
+        m = []
+        keyword.each_char do |c|
+            if c=='j'   # => troca o 'j' pelo 'i'
+                c='i'
+            end
+            if ! m.include? c
+                m << c
+            end
+        end
+        ("a".."z").each do |c|
+            if c=='j'   # => troca o 'j' pelo 'i'
+                c='i'
+            end
+            if ! m.include? c
+                m << c
+            end
+        end
+        return m
+    end
+
+    def criaMatrizChave(array, ordem)
+        m = Array.new(ordem)
+        i=0
+        array.each do |c|
+            if i<ordem
+                if m[i].nil?
+                    m[i]=[]
+                end
+                if m[i].length<ordem
+                    m[i] << c
+                else
+                    if (i+=1)<ordem
+                        if m[i].nil?
+                            m[i]=[]
+                        end
+                        m[i] << c
+                    end
+                end
+            end
+        end
+        return m
+    end
+    
+    def imprimeMatriz(m)
+        m.each do |linha|
+            linha.each do |elemento|
+                print elemento+' '
+            end
+            puts
         end
     end
-end
-matriz.each do |c|
-    print c
-end
+    
+# }
+
+# main {
+    if ARGV.length < 2
+        abort "escreve os argumentos babaca"
+    end
+    n=ARGV[0].to_i
+    keyword=ARGV[1]
+    
+    matchave = criaMatrizChave(criaArrayChave(keyword), 5)
+# }
